4187d98341447d27f99e656a21b6a7ef108b9975
AlokPrasanna <alokprasanna1104@gmail.com>
2024-12-25 08:22:53 +0530

    Crated Server

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// -------------------- Third-party components and modules --------------------
+const express = require("express");
+require("dotenv/config");
+
+// -------------------- Custom Library and Modules -------------------
+const Config = require("./config");
+
+// -------------------- Third-pary modules component and modules --------------------
+const app = express();
+const PORT = Config.PORT || 3308;
+
+// -------------------- Common Middleware --------------------
+app.use(express.json());
+
+// -------------------- Base Route --------------------
+app.get("/" , (req, res) => {
+    res.status(200).json({
+        status: true,
+        message: "Welcome to the server!"
+    });
+});
+
+//-------------------- Error Route --------------------
+app.use((req , res) => {
+    res.status(404).json({
+        status: false,
+        message: "Not Found!"
+    });
+});
+
+// -------------------- Initialize Connection --------------------
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT} port`);
+});
