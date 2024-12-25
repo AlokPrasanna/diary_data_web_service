71712340a876cb652503740aa553a9c9e40e4fb4
AlokPrasanna <alokprasanna1104@gmail.com>
2024-12-25 20:34:57 +0530

    Created Get All Data function route

--- src/api/v1/routes/Diary.js
+++ src/api/v1/routes/Diary.js
@@ -3,7 +3,8 @@ const express = require("express");
 
 // -------------------- Custom Library and Modules --------------------
 const {
-    SaveDiary
+    SaveDiary,
+    GetAllData,
 } = require("../controllers");
 
 // ---------- Initilaize the Router ----------
@@ -12,6 +13,9 @@ const router = express.Router();
 // ----- Routes -----
 // Save Diary Data
 router.post("/save" , SaveDiary);
+
+// Get All data
+router.get("/all" , GetAllData);
 
 
 module.exports = router;
